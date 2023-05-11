525bf64a20e0c05445474a82931276222f895b23
Roma Kolesnyk <romakoles1@ukr.net>
2023-05-11 14:59:10 +0300

    5 functions 100% predict fork join task + pool

--- src/main/java/com/kpi/kolesnyk/gmdh/GMDH.java
+++ src/main/java/com/kpi/kolesnyk/gmdh/GMDH.java
@@ -2,9 +2,8 @@ package com.kpi.kolesnyk.gmdh;
 
 import java.time.Duration;
 import java.time.Instant;
-import java.util.Arrays;
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.*;
 import java.util.stream.Collectors;
 
 public class GMDH {
@@ -42,22 +41,31 @@ public class GMDH {
         };
 
         double[][] inputMatrixBig = {
-                {5.2, 27.04, 140.608, 856.6464},
-                {9.3, 86.49, 804.357, 3269.4021},
-                {149, 22201, 3307949, 6390368.35},
-                {-90.6, 8208.36, -743677.416, -1179373.8708},
-                {142.9, 20420.41, 2918076.589, 5653704.5157},
-                {-300.1, 90060.01, -27027009.001, -46892020.1663},
-                {3, 9, 27, 269.55},
-                {9.01, 81.1801, 731.432701, 3030.3858168}
+                {5.20,27.04,140.61,731.16,32.66,7053.91},
+                {9.30,86.49,804.36,7480.52,29.20,69622.42},
+                {11.90,141.61,1685.16,20053.39,18.68,185510.01},
+                {-9.06,82.08,-743.68,6737.72,-56.90,64782.66},
+                {1.43,2.04,2.92,4.17,4.49,84.00},
+                {-3.90,15.22,-59.36,231.58,-6.12,2482.62},
+                {3.10,9.61,29.79,92.35,19.47,984.67},
+                {9.01,81.18,731.43,6590.21,28.29,61406.58},
+                {14.39,207.07,2979.77,42878.85,22.59,395527.37},
+                {-2.99,8.95,-26.76,80.03,-18.78,971.15},
+                {4.10,16.81,68.92,282.58,12.87,2861.17},
+                {10.01,100.20,1003.00,10040.06,15.72,93275.23}
         };
 
-        Set<Matrix> candidates = new ModelContainer(inputMatrixBig)
-                .getCandidates()
+        Set<Matrix> candidates1 = new ModelContainer(inputMatrixBig)
+                .getCandidates();
+
+        System.out.println(candidates1.size());
+        Set<Matrix> candidates = candidates1
                 .stream()
-                .limit(1)
+//                .limit(1)
                 .map(GMDH::findB)
-                .collect(Collectors.toSet());
+                .sorted(Comparator.comparing(Matrix::getRegularityCriterion))
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+
 
         printCandidateStatistics(candidates);
         System.out.printf("time elapsed = %d ms", Duration.between(start, Instant.now()).toMillis());
@@ -74,7 +82,8 @@ public class GMDH {
     }
 
     private static class ModelContainer {
-        private final Set<Matrix> candidates = new LinkedHashSet<>();
+        private final Set<Matrix> candidates = Collections.synchronizedSet(new HashSet<>());
+        private final ForkJoinPool pool = new ForkJoinPool();
 
         public ModelContainer(double[][] complexModel) {
             simplifyComplexMatrix(new Matrix(complexModel));
@@ -90,17 +99,49 @@ public class GMDH {
          *                      ]
          */
         private void simplifyComplexMatrix(Matrix complexMatrix) {
-            candidates.add(complexMatrix);
-            int basedFunctions = complexMatrix.getNcols() - 1;
-            if (basedFunctions <= 1) {
-                return;
+            StarRecursiveAction task = new StarRecursiveAction(complexMatrix, candidates);
+            pool.execute(task);
+            task.join();
+        }
+
+        private static class StarRecursiveAction extends RecursiveAction {
+            private static final int THRESHOLD = 10;
+            private final Matrix complexMatrix;
+            private final Set<Matrix> candidates;
+
+            public StarRecursiveAction(Matrix complexMatrix, Set<Matrix> candidates) {
+                this.complexMatrix = complexMatrix;
+                this.candidates = candidates;
             }
-            for (int columnNumber = 0; columnNumber < basedFunctions; columnNumber++) {
-                simplifyComplexMatrix(removeColumn(complexMatrix, columnNumber));
+
+            private void simplifyRecursive(Matrix complexMatrix) {
+                candidates.add(complexMatrix);
+                int basedFunctions = complexMatrix.getNcols() - 1;
+                if (basedFunctions <= 1) {
+                    return;
+                }
+                for (int columnNumber = 0; columnNumber < basedFunctions; columnNumber++) {
+                    simplifyRecursive(removeColumn(complexMatrix, columnNumber));
+                }
+            }
+
+            @Override
+            protected void compute() {
+                candidates.add(complexMatrix);
+                int basedFunctions = complexMatrix.getNcols() - 1;
+                if (basedFunctions <= THRESHOLD) {
+                    simplifyRecursive(complexMatrix);
+                    return;
+                }
+                for (int columnNumber = 0; columnNumber < basedFunctions; columnNumber++) {
+                    ForkJoinTask.invokeAll(
+                            new StarRecursiveAction(removeColumn(complexMatrix, columnNumber), candidates)
+                    );
+                }
             }
         }
 
-        private Matrix removeColumn(Matrix complexMatrix, int columnNumber) {
+        private static Matrix removeColumn(Matrix complexMatrix, int columnNumber) {
             return new Matrix(cloneWithNoColumn(complexMatrix.getValues(), columnNumber));
         }
 
@@ -130,38 +171,38 @@ public class GMDH {
         double[][] trainData = Arrays.copyOfRange(inputMatrix, 0, (n + 1) / 2);
         double[][] checkData = Arrays.copyOfRange(inputMatrix, (n + 1) / 2, n);
 
-        System.out.println("input train matrix");
-        System.out.println(Arrays.deepToString(trainData));
-        System.out.println();
+//        System.out.println("input train matrix");
+//        System.out.println(Arrays.deepToString(trainData));
+//        System.out.println();
 
-        System.out.println("X");
+//        System.out.println("X");
         double[][] X = provideXMatrix(trainData);
         Matrix matrix = new Matrix(X);
-        matrix.print();
+//        matrix.print();
 
-        System.out.println("Xt");
+//        System.out.println("Xt");
         Matrix transpose = MatrixMathematics.transpose(matrix);
-        transpose.print();
+//        transpose.print();
 
-        System.out.println("Xt * X");
+//        System.out.println("Xt * X");
         Matrix multiply = MatrixMathematics.multiply(transpose, matrix);
-        multiply.print();
+//        multiply.print();
 
-        System.out.println("Xt * X ^ -1");
+//        System.out.println("Xt * X ^ -1");
         Matrix inverse = MatrixMathematics.inverse(multiply);
-        inverse.print();
+//        inverse.print();
 
-        System.out.println("Xt * X ^ -1 * Xt");
+//        System.out.println("Xt * X ^ -1 * Xt");
         Matrix multiply1 = MatrixMathematics.multiply(inverse, transpose);
-        multiply1.print();
+//        multiply1.print();
 
-        System.out.println("Y");
+//        System.out.println("Y");
         Matrix Y = new Matrix(provideYMatrix(trainData));
-        Y.print();
+//        Y.print();
 
-        System.out.println("Xt * X ^ -1 * Xt * Y");
+//        System.out.println("Xt * X ^ -1 * Xt * Y");
         Matrix b = MatrixMathematics.multiply(multiply1, Y);
-        b.print();
+//        b.print();
         candidate.setB(b);
 
         candidate.setRegularityCriterion(calculateRegularityCriterion(checkData, b));
